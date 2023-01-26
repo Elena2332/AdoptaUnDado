e64f7376bf2a5645f7177be3f97335be0340912b
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2023-01-26 09:24:51 +0100

    Pool de conexiones hecho y librerías importadas

---
+++ src/main/java/conex/ConnectionPoolDB.java
@@ -0,0 +1,29 @@
+package conex;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionPoolDB {
+	private static DataSource dataSource;
+
+	public static DataSource getDataSource() {
+
+		if (dataSource == null) {
+			try {
+				InitialContext ctx = new InitialContext();
+				Context env = (Context) ctx.lookup("java:comp/env");
+				// nombre del recurso en el context.xml
+				dataSource = (DataSource) env.lookup("jdbc/poolDadosDB");
+
+			} catch (NamingException e) {
+				e.printStackTrace();
+
+			}
+		}
+
+		return dataSource;
+	}
+
+}
